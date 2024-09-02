041171f62b2eeb4f03bdd4904477e24dabad5d59
RossTsachev <ross102t@gmail.com>
2024-09-02 07:39:26 +0300

    Add Task 5 from Homework 1

---
+++ src/main/java/Homework1/Task05.java
@@ -0,0 +1,18 @@
+package Homework1;
+
+public class Task05 {
+    public static void main( String[] args ) {
+
+        System.out.println("Printing numbers from 1 to 100:");
+
+        for (int i=1; i <= 100; i++) {
+            System.out.println(i);
+        }
+
+        System.out.println("Printing numbers from 100 to 1:");
+
+        for (int i=100; i >= 1; i--) {
+            System.out.println(i);
+        }
+    }
+}
